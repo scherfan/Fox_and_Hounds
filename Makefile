2e2fcd2559d53487c8b4d3b775c65049fcdd8100
scherfan <cherfan@ualberta.ca>
2014-02-16 18:46:57 -0700

    Changed game_board.cpp to game.cpp and updated Makefile accordingly. Also moved all #inlcudes to .h file.

--- Makefile
+++ Makefile
@@ -1,13 +1,13 @@
 # Makefile for Fox_and_Hounds
 
-fox_and_hounds: main.o game_board.o
-	g++ -Wall main.o game_board.o -o fox_and_hounds
+fox_and_hounds: main.o game.o
+	g++ -Wall main.o game.o -o fox_and_hounds
 
 main.o: main.cpp fox_and_hounds.h
 	g++ -c main.cpp
 
-game_board.o: game_board.cpp fox_and_hounds.h
-	g++ -c game_board.cpp
+game_board.o: game.cpp fox_and_hounds.h
+	g++ -c game.cpp
 
 clean:
 	rm -rf *.o *.cpp~ *.h~ fox_and_hounds core
---
+++ game.cpp
@@ -0,0 +1,58 @@
+/*
+  Will contain all the class and functions for drawing the gameboard and moving the
+  pieces.
+ */
+
+#include "fox_and_hounds.h"
+
+using namespace std;
+
+class Game
+{
+    
+}
+
+void draw_board()
+{
+    cout << "-------------------------------------------------" << endl;
+    cout << "|     |xxxxx|     |xxxxx|     |xxxxx|     |xxxxx|" << endl;
+    cout << "|     |x " << "x" << " x|     |x " << "x" << " x|     |x " 
+         << "x" << " x|     |x " << "x" << " x|" << endl;
+    cout << "|     |xxxxx|     |xxxxx|     |xxxxx|     |xxxxx|" << endl;
+    cout << "-------------------------------------------------" << endl;
+    cout << "|xxxxx|     |xxxxx|     |xxxxx|     |xxxxx|     |" << endl;
+    cout << "|x " << "x" << " x|     |x " << "x" << " x|     |x " << "x" 
+         << " x|     |x " << "x" << " x|     |" << endl;
+    cout << "|xxxxx|     |xxxxx|     |xxxxx|     |xxxxx|     |" << endl;
+    cout << "-------------------------------------------------" << endl;
+    cout << "|     |xxxxx|     |xxxxx|     |xxxxx|     |xxxxx|" << endl;
+    cout << "|     |x " << "x" << " x|     |x " << "x" << " x|     |x " 
+         << "x" << " x|     |x " << "x" << " x|" << endl;
+    cout << "|     |xxxxx|     |xxxxx|     |xxxxx|     |xxxxx|" << endl;
+    cout << "-------------------------------------------------" << endl;
+    cout << "|xxxxx|     |xxxxx|     |xxxxx|     |xxxxx|     |" << endl;
+    cout << "|x " << "x" << " x|     |x " << "x" << " x|     |x " << "x"
+         << " x|     |x " << "x" << " x|     |" << endl;
+    cout << "|xxxxx|     |xxxxx|     |xxxxx|     |xxxxx|     |" << endl;
+    cout << "-------------------------------------------------" << endl;
+    cout << "|     |xxxxx|     |xxxxx|     |xxxxx|     |xxxxx|" << endl;
+    cout << "|     |x " << "x" << " x|     |x " << "x" << " x|     |x " 
+         << "x" << " x|     |x " << "x" << " x|" << endl;    
+    cout << "|     |xxxxx|     |xxxxx|     |xxxxx|     |xxxxx|" << endl;
+    cout << "-------------------------------------------------" << endl;
+    cout << "|xxxxx|     |xxxxx|     |xxxxx|     |xxxxx|     |" << endl;
+    cout << "|x " << "x" << " x|     |x " << "x" << " x|     |x " << "x" 
+         << " x|     |x " << "x" << " x|     |" << endl;
+    cout << "|xxxxx|     |xxxxx|     |xxxxx|     |xxxxx|     |" << endl;
+    cout << "-------------------------------------------------" << endl;
+    cout << "|     |xxxxx|     |xxxxx|     |xxxxx|     |xxxxx|" << endl;
+    cout << "|     |x " << "x" << " x|     |x " << "x" << " x|     |x " 
+         << "x" << " x|     |x " << "x" << " x|" << endl;
+    cout << "|     |xxxxx|     |xxxxx|     |xxxxx|     |xxxxx|" << endl;
+    cout << "-------------------------------------------------" << endl;
+    cout << "|xxxxx|     |xxxxx|     |xxxxx|     |xxxxx|     |" << endl;
+    cout << "|x " << "x" << " x|     |x " << "x" << " x|     |x " << "x" 
+         << " x|     |x " << "x" << " x|     |" << endl;
+    cout << "|xxxxx|     |xxxxx|     |xxxxx|     |xxxxx|     |" << endl;
+    cout << "-------------------------------------------------" << endl;
+}
