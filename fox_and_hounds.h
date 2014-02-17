6e808ce3dd1e02a17440056e24e80b891c42c744
scherfan <cherfan@ualberta.ca>
2014-02-16 20:58:41 -0700

    Added the class and enums to the .h file.

--- fox_and_hounds.h
+++ fox_and_hounds.h
@@ -1,12 +1,25 @@
 /*
   Fox_and_hounds .h file.
 */
+
 #include <iostream>
 #include <string>
 
 #ifndef FOXANDHOUNDS_H
 #define FOXANDHOUNDS_H
-void draw_board();
-void move_hound(int hnum, char dir);
-void move_fox(char dir);
+
+enum Game_Piece{HOUND1, HOUND2, HOUND3, HOUND4, FOX, EMPTY_SQUARE, INVALID_SQUARE};
+enum Player_Type{PT_FOX, PT_HOUND};
+
+class Game_Board
+{
+public:
+    Game_Board();
+    void move_fox(char dir1, char dir2);
+    void move_hound(int hnum, char dir1, char dir2);
+    void draw_board();
+private:
+    Game_Piece _board[8][8];
+    Player_Type _whose_move;
+};
 #endif
