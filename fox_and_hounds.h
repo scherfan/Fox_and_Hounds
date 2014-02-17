056bd26ebfbdf3bf32e747d64c29b7638cc85dba
scherfan <cherfan@ualberta.ca>
2014-02-16 18:49:05 -0700

    main.cpp now asks for player input.

--- fox_and_hounds.h
+++ fox_and_hounds.h
@@ -1,8 +1,12 @@
 /*
   Fox_and_hounds .h file.
 */
+#include <iostream>
+#include <string>
 
 #ifndef FOXANDHOUNDS_H
 #define FOXANDHOUNDS_H
 void draw_board();
+void move_hound(int hnum, char dir);
+void move_fox(char dir);
 #endif
--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
   Main file.
  */
 
-#include <iostream>
 #include "fox_and_hounds.h"
 
 using namespace std;
@@ -10,11 +9,23 @@ using namespace std;
 int main()
 {
     char quit;
-
+    string fox_move;
+    string hound_move;
+    
+    cout << endl << "FOX AND HOUNDS" << endl << endl;
+    cout << "Player 1 is the Fox and player 2 is the Hounds" << endl;
+    draw_board();
     while(true)
     {
-        cout << "the game board:" << endl;
-        draw_board();
+        cout << "To move the Fox just type:" << endl << "fl = forward left" << endl
+             << "fr = forward right" << endl << "bl = backwards left" << endl
+             << "br = backwards right" << endl;
+        cin >> fox_move;
+        //execute fox's move
+        cout << "To move a Hound just type the number of the Hound and:" << endl
+             << "fl = forward left" << endl << "fr = forward right" << endl;
+        cin >> hound_move;
+        //execute hound's move
         cout << "Would you like to quit?: (y or n)";
         cin >> quit;
         if (quit == 'y')
